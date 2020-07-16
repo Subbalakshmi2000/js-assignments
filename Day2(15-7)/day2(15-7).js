e426b6cbeaed1c20c40a0d061f1d88b163f1db9d
Subbalakshmi2000 <50201352+Subbalakshmi2000@users.noreply.github.com>
2020-07-16 13:02:35 +0530

    Add files via upload

---
+++ Day2(15-7)/day2(15-7).js
@@ -0,0 +1,12 @@
+//Promt() method in Javascript...
+//The prompt() method is used to display a dialog with an optional message prompting the user to input some text. 
+//It is often used if the user wants to input a value before entering a page.
+//It returns a string containing the text entered by the user, or null.
+
+//Syntax:
+//prompt(title, default)
+//title is a string of text to display to the user.It can be omitted if there is nothing to show in the prompt window i.e. it is optional.
+//default is a string containing the default value displayed in the text input field. It is also optional
+
+let age = prompt("What is your age?");
+console.log(age);
